0558eabcad041d5c5a1905728c550c2479dd3ef3
Courtney <courtney@CE-Air.fios-router.home>
2021-04-30 12:24:18 -0400

    create-react-app -> make front end dynamic with template data

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Nike Slim Shirt",
+      category: "Shirts",
+      image: "/images/product-1.jpg",
+      price: 120,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product"
+    },
+    {
+      _id: "2",
+      name: "Adidas Fit Shirt",
+      category: "Shirts",
+      image: "/images/product-2.jpg",
+      price: 100,
+      brand: "Adidas",
+      rating: 5.0,
+      numReviews: 6,
+      description: "high quality adidas shirt"
+    },
+    {
+      _id: "3",
+      name: "Lacoste Free Shirt",
+      category: "Shirts",
+      image: "/images/product-3.jpg",
+      price: 90,
+      brand: "Lacoste",
+      rating: 4,
+      numReviews: 17,
+      description: "high quality lacoste shirt"
+    },
+    {
+      _id: "4",
+      name: "Nike Slim Pant",
+      category: "Pants",
+      image: "/images/product-4.jpg",
+      price: 78,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 7,
+      description: "high quality nike pants"
+    },
+    {
+      _id: "5",
+      name: "Puma Slim Pant",
+      category: "Pants",
+      image: "/images/product-5.jpg",
+      price: 100,
+      brand: "Puma",
+      rating: 4.0,
+      numReviews: 32,
+      description: "high quality puma pants"
+    },
+    {
+      _id: "6",
+      name: "Adidas Fit Shirt",
+      category: "Pants",
+      image: "/images/product-6.jpg",
+      price: 75,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 32,
+      description: "high quality puma pants"
+    }
+  ]
+}
+
+export default data;
